f31bd0ab35fb96483a45a195cf7d4e46f655430b
Frank Li <frank.li.dev@gmail.com>
2021-12-19 20:23:34 -0800

    add detect script

---
+++ savio_jobs/video_detect/production_job_gpu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=video-detect-job
+#
+# Account:
+#SBATCH --account=fc_control
+#
+# Partition:
+#SBATCH --partition=savio3_gpu
+#
+# Number of nodes:
+#SBATCH --nodes=1
+#
+# Number of tasks (one for each GPU desired for use case) (example):
+#SBATCH --ntasks=1
+#
+# Processors per task (please always specify the total number of processors twice the number of GPUs):
+#SBATCH --cpus-per-task=4
+#
+#Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
+#SBATCH --gres=gpu:GTX2080TI:1
+#
+# Wall clock limit:
+#SBATCH --time=12:00:00
+#
+## Command(s) to run (example):
+
+#readme
+#please define the following variable
+export VIDEOS_ABS_PATH=path_to_data_folder
+
+module load python gcc opencv cmake
+pip install --user --upgrade pip setuptools wheel && pip install --user -r ~/curb-monitor/requirements.txt
+
+# run job
+cd ~/curb-monitor && python ./scripts/detect_objects.py --save-csv --class 1 3 4 5 6 --conf-thres 0.25 --img 640  --weights ./models/curb_prod_model/weights/best.pt --source $VIDEOS_ABS_PATH
