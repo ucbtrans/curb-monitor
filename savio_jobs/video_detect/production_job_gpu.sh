43bcb729a18fe1fd9de912ecfa916039d9f6a1d6
frankdvd <frankdvd@users.noreply.github.com>
2021-12-21 22:52:03 -0800

    Update production_job_gpu.sh

--- savio_jobs/video_detect/production_job_gpu.sh
+++ savio_jobs/video_detect/production_job_gpu.sh
@@ -27,10 +27,11 @@
 
 #readme
 #please define the following variable
-export VIDEOS_ABS_PATH=path_to_data_folder
+export VIDEOS_ABS_PATH=path_to_data_folder #/global/scratch/users/sidali/bus_vids_10_20_21
+export OUT_PUT_PATH=path_to_output_folder #/global/scratch/users/sidali/runs/detect
 
 module load python gcc opencv cmake
 pip install --user --upgrade pip setuptools wheel && pip install --user -r ~/curb-monitor/requirements.txt
 
 # run job
-cd ~/curb-monitor && python ./scripts/detect_objects.py --save-csv --class 1 3 4 5 6 --conf-thres 0.25 --img 640  --weights ./models/curb_prod_model/weights/best.pt --source $VIDEOS_ABS_PATH
+cd ~/curb-monitor && python ./scripts/detect_objects.py --project $OUT_PUT_PATH --save-csv --class 1 3 4 5 6 --conf-thres 0.25 --img 640  --weights ./models/curb_prod_model/weights/best.pt --source $VIDEOS_ABS_PATH
