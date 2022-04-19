c70b9d856f304ea26aa300917809e82b09123699
Aravinth1997 <aravinth_paranan@berkeley.edu>
2022-04-18 19:29:00 -0700

    Changed Date format and changes in swagger

---
+++ flask_apis/constants.py
@@ -0,0 +1,5 @@
+return_object_keys = {"weekday": "weekday", "date": "date", "h": "box_h", "x": "box_x", "w": "box_w", "y": "box_y",
+                      "box_area": "box_area",
+                      "time": "time", "is_on_bus_line": "is_on_bus_line", "obj_class_name": "obj_class_name",
+                      "longitude": "longitude", "latitude": "latitude", "speed": "speed",
+                      "confidences": "confidences",  "original_file" : "video_filename"}
